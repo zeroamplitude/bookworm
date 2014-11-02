b73bc0493825f770588cfee352d6f9e1b45109d3
beastMode <nicholas.desouza@uoit.net>
2014-11-01 20:49:42 -0400

    changed config settings

--- config.py
+++ config.py
@@ -9,13 +9,13 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
-class ProductionConfig(Config):
-    DEBUG = False
-
-
-class StagingConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
+# class ProductionConfig(Config):
+#     DEBUG = False
+#
+#
+# class StagingConfig(Config):
+#     DEVELOPMENT = True
+#     DEBUG = True
 
 
 class DevelopmentConfig(Config):
