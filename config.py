46e676228ade1162117aa65d6de5ae61772d8b24
beastMode <nicholas.desouza@uoit.net>
2014-11-02 02:02:16 -0500

    added OpenID login info and skeleton for Facebook login

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
 WTF_CSRF_ENABLED = True
 SECRET_KEY ='you-will-never-guess'
+
+OPEID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/08/id'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
+]
