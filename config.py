2ec2b7a2f30c4c6b1cce2d75f90e1e0cecd5ed25
beastMode <nicholas.desouza@uoit.net>
2014-11-01 21:04:30 -0400

    added database requirements and URI

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'HelloWorld'
-    #SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
 # class ProductionConfig(Config):
