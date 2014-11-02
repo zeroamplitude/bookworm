1ef5e647fd2053895e7f0a3f99671fac703d4bab
beastMode <nicholas.desouza@uoit.net>
2014-11-02 17:20:56 -0500

    add migration features: Upgrade, Downgrade

--- config.py
+++ config.py
@@ -1,7 +1,14 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'beastMode'
 
-OPEID_PROVIDERS = [
+OPENID_PROVIDERS = [
     {'name': 'Google', 'url': 'https://www.google.com/accounts/08/id'},
     {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
