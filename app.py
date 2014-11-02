280906bb9968ffc938d97b423ce8b1e5da27001c
beastMode <nicholas.desouza@uoit.net>
2014-11-01 20:59:27 -0400

    fixed directory structure

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import os
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 
+print os.environ['APP_SETTINGS']
 
 @app.route('/')
 def hello():
--- config.py
+++ config.py
@@ -5,8 +5,8 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'this-really-needs-to-be-changed'
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SECRET_KEY = 'HelloWorld'
+    #SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
 # class ProductionConfig(Config):
