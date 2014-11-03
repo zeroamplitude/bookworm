c4affdb9d3c12f9437f4878dce9c38753e56f059
beastMode <nicholas.desouza@uoit.net>
2014-11-02 21:16:24 -0500

    added edit profile feature

--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid
-from .forms import LoginForm
+from forms import LoginForm, EditForm
 from models import User
+from datetime import datetime
 
 @app.route('/')
 @app.route('/index')
@@ -67,6 +68,10 @@ def after_login(resp):
 @app.before_request
 def before_request():
     g.user = current_user
+    if g.user.is_authenticated():
+        g.user.last_seen = datetime.utcnow()
+        db.session.add(g.user)
+        db.session.commit()
 
 @app.route('/logout')
 def logout():
@@ -87,3 +92,19 @@ def user(nickname):
     return render_template('user.html',
                            user=user,
                            posts=posts)
+
+@app.route('/edit', methods=['GET', 'POST'])
+@login_required
+def edit():
+    form = EditForm()
+    if form.validate_on_submit():
+        g.user.nickname = form.nickname.data
+        g.user.about_me = form.about_me.data
+        db.session.add(g.user)
+        db.session.commit()
+        flash('Your profile has been updated.')
+        return redirect(url_for('edit'))
+    else:
+        form.nickname.data = g.user.nickname
+        form.about_me.data = g.user.about_me
+    return render_template('edit.html', form=form)
