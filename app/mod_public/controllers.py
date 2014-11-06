093c1d6b4bd40c4c660f0a2dacc47b05bd7f8bd0
beastMode <nicholas.desouza@uoit.net>
2014-11-06 13:43:21 -0500

    added search functionality

--- app/mod_public/controllers.py
+++ app/mod_public/controllers.py
@@ -1,19 +1,26 @@
-from flask import render_template, request, Blueprint
-from app import mod_public
-from app.mod_public.forms import ContactForm
+from flask import render_template, request, Blueprint, session
+from app import mod_public, db
+from app.mod_public.forms import ContactForm, SearchForm
+from app.models import Book
 
 mod_public = Blueprint('public', __name__, url_prefix='/')
 
 # Home - Home page
 @mod_public.route('/')
-@mod_public.route('home/')
+@mod_public.route('home/', methods=['GET', 'POST'])
 def home():
-    return render_template('public/home.html')
+    form = SearchForm()
+    if request.method == 'POST':
+        return render_template('public/home.html', form=form)
+    else:
+        return render_template('public/home.html', form=form)
+
 
 # About - Provides basic information about bookworm
 @mod_public.route('about/')
 def about():
     return render_template('public/about.html')
+
 
 # Contact - Allows user to contact web admin
 @mod_public.route('contact/', methods=['GET', 'POST'])
@@ -28,3 +35,14 @@ def contact():
             return 'Form posted.'
     elif request.method == 'GET':
         return render_template('public/contact.html', form=form)
+
+
+@mod_public.route('search/', methods=['Get', 'Post'])
+def search():
+    form = SearchForm()
+    if request.method == 'POST':
+        books = Book.query.filter(Book.title == form.title.data)
+        return render_template('public/home.html', books=books, form=form)
+    else:
+        return render_template('public/home.html', form=form)
+
--- app/mod_public/forms.py
+++ app/mod_public/forms.py
@@ -9,3 +9,11 @@ class ContactForm(Form):
     subject = StringField("Subject", [validators.DataRequired("Please enter a subject")])
     message = TextAreaField("Message", [validators.DataRequired("Please enter your message")])
     submit = SubmitField("Send")
+
+
+class SearchForm(Form):
+    title = StringField("Title")
+    submit =SubmitField("Search")
+
+    def __init__(self, *args, **kwargs):
+        Form.__init__(self, *args, **kwargs)
