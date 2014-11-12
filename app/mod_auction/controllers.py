fec9cc55ebb7d0762a9707ac0d7228531147281d
beastMode <nicholas.desouza@uoit.net>
2014-11-12 11:08:24 -0500

    Bid: Displays current highbid

--- app/mod_auction/controllers.py
+++ app/mod_auction/controllers.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, session, redirect, url_for, request, render_template, flash
+from sqlalchemy import func
 from app import db
 from app.mod_auction.forms import BidForm
 from app.mod_member.forms import UploadBookForm
@@ -13,11 +14,11 @@ def book(bookID):
     form = BidForm()
     bookS = db.session.query(Book).filter(Book.book_id == bookID).scalar()
     aucID = db.session.query(Book.auc_id).filter(Book.book_id == bookID).scalar()
-    # curPrice = db.session.query(Bid.bid_price).filter(Bid.bid_price.max())
+    curPrice = db.session.query(func.max(Bid.bid_price)).scalar()
     if request.method == 'POST':
         if not form.validate():
             flash('Bid Unsuccessful')
-            return render_template('auction/book.html', form=form, book=bookS)
+            return render_template('auction/book.html', form=form, book=bookS, curPrice=curPrice)
         else:
             if 'email' not in session:
                 return redirect(url_for('auth.signin'))
@@ -27,15 +28,15 @@ def book(bookID):
                     filter(Bid.bid_price >= form.bid_price.data).first()
                 if highBid:
                     flash('Your bid needs to be higher then the current price')
-                    return render_template('auction/book.html', form=form, book=bookS)
+                    return render_template('auction/book.html', form=form, book=bookS, curPrice=curPrice)
                 else:
                     newBid = Bid(auc_id=aucID, user_id=usrID, bid_price=form.bid_price.data)
                     db.session.add(newBid)
                     db.session.commit()
                     flash('Bid Successful')
-                return render_template('auction/book.html', form=form, book=bookS)
+                return render_template('auction/book.html', form=form, book=bookS, curPrice=form.bid_price.data)
     
     elif request.method == 'GET':
-        return render_template('auction/book.html', form=form, book=bookS)
+        return render_template('auction/book.html', form=form, book=bookS, curPrice=curPrice)
 
 
