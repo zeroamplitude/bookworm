71eaa9cad8f16b6bfc29c27d71673a3117d20324
beastMode <nicholas.desouza@uoit.net>
2014-11-12 10:37:37 -0500

    Bid: Validates user to make sure they are in session

--- app/mod_auction/controllers.py
+++ app/mod_auction/controllers.py
@@ -12,24 +12,28 @@ mod_auction = Blueprint('auction', __name__, url_prefix='/auction')
 def book(bookID):
     form = BidForm()
     bookS = db.session.query(Book).filter(Book.book_id == bookID).scalar()
+    aucID = db.session.query(Book.auc_id).filter(Book.book_id == bookID).scalar()
+    # curPrice = db.session.query(Bid.bid_price).filter(Bid.bid_price.max())
     if request.method == 'POST':
         if not form.validate():
             flash('Bid Unsuccessful')
             return render_template('auction/book.html', form=form, book=bookS)
         else:
-            aucID = db.session.query(Book.auc_id).filter(Book.book_id == bookID).scalar()
-            usrID = db.session.query(User.user_id).filter(User.email == session['email']).scalar()
-            highBid = db.session.query(Bid.bid_price).filter(Bid.auc_id == aucID).\
-                filter(Bid.bid_price >= form.bid_price.data).first()
-            if highBid:
-                flash('Your bid needs to be higher then the current price')
-                return render_template('auction/book.html', form=form, book=bookS)
+            if 'email' not in session:
+                return redirect(url_for('auth.signin'))
             else:
-                newBid = Bid(auc_id=aucID, user_id=usrID, bid_price=form.bid_price.data)
-                db.session.add(newBid)
-                db.session.commit()
-                flash('Bid Successful')
-            return render_template('auction/book.html', form=form, book=bookS)
+                usrID = db.session.query(User.user_id).filter(User.email == session['email']).scalar()
+                highBid = db.session.query(Bid.bid_price).filter(Bid.auc_id == aucID).\
+                    filter(Bid.bid_price >= form.bid_price.data).first()
+                if highBid:
+                    flash('Your bid needs to be higher then the current price')
+                    return render_template('auction/book.html', form=form, book=bookS)
+                else:
+                    newBid = Bid(auc_id=aucID, user_id=usrID, bid_price=form.bid_price.data)
+                    db.session.add(newBid)
+                    db.session.commit()
+                    flash('Bid Successful')
+                return render_template('auction/book.html', form=form, book=bookS)
     
     elif request.method == 'GET':
         return render_template('auction/book.html', form=form, book=bookS)
